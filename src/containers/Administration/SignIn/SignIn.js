beba518cbc076fa8a6fabcfb352c5d739359a34e
Vishal Chaudhary <vishal.chaudhary@kuliza.com>
2020-07-26 18:41:20 +0530

    created Input Component

--- src/containers/Administration/SignIn/SignIn.js
+++ src/containers/Administration/SignIn/SignIn.js
@@ -3,46 +3,189 @@ import Aux from '../../../hoc/Aux/Aux'
 import classes from './SignIn.module.css'
 import axios from '../../../Axios/axios-userAuth'
 import { Link } from 'react-router-dom'
+import Spinner from '../../../components/UI/Spinner/Spinner'
+import Input from '../../../components/UI/Input/Input'
 class SignIn extends Component {
 
+    // state = {
+    //     signInInfo: {
+    //         username: '',
+    //         password: ''            
+    //     }   
+    // }    
+
     state = {
-        signInInfo: {
-            username: '',
-            password: ''            
-        }   
-    }    
-
-    changeHandler = (event) => {
-        const signInInfo = {...this.state.signInInfo}        
-        const name = event.target.name
-        const val = event.target.value
-
-        if(name === 'username')
-        signInInfo.username = val
-        
-        if(name === 'password')
-        signInInfo.password = val
-
-        this.setState({signInInfo: signInInfo})
+        signInCred: {
+            username: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Username'
+                },
+                value: '',
+                validation: {
+                    required: true,                    
+                },
+                valid: false,
+                touched: false
+            },
+            password: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'password',
+                    placeholder: 'Your Password'
+                },
+                value: '',
+                validation: {
+                    required: true,                    
+                },
+                valid: false,
+                touched: false
+            }
+        },
+        formIsValid: false,
+        loading: false
     }
 
-    submitClicked = () => {
-        const signInInfo = this.state.signInInfo
+    checkValidity(value, rules) {
+        let isValid = true
+
+        if(rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        // if(rules.minLength) {
+        //     isValid = value.length >= rules.minLength && isValid;
+        // }
+
+        // if(rules.maxLength) {
+        //     isValid = value.length <= rules.maxLength && isValid;
+        // }
+
+        return isValid
+
+    }
+
+    inputChangeHandler = (event, inputIdentifier) => {
+        const updatedSignInForm = {
+            ...this.state.signInCred
+        }
+
+        const updatedSignInElement = {
+            ...updatedSignInForm[inputIdentifier]
+        }
+
+        updatedSignInElement.value = event.target.value
+        updatedSignInElement.valid = this.checkValidity(updatedSignInElement.value, updatedSignInElement.validation)
+        updatedSignInElement.touched = true
+        updatedSignInForm[inputIdentifier] = updatedSignInElement
+
+        let formIsValid = true
+        for(let inputIdentifier in updatedSignInForm) {
+            formIsValid = updatedSignInForm[inputIdentifier].valid && formIsValid
+        }
+
+        this.setState({signInCred: updatedSignInForm, formIsValid: formIsValid})
+    }
+
+    // changeHandler = (event) => {
+    //     const signInInfo = {...this.state.signInInfo}        
+    //     const name = event.target.name
+    //     const val = event.target.value
+
+    //     if(name === 'username')
+    //     signInInfo.username = val
+        
+    //     if(name === 'password')
+    //     signInInfo.password = val
+
+    //     this.setState({signInInfo: signInInfo})
+    // }
+
+    submitClicked = (event) => {
+
+        event.preventDefault()
+        this.setState({loading: true})
+
+        const formData = {}
+        
+        for(let formElementIdentifier in this.state.signInCred) {
+            formData[formElementIdentifier] = this.state.signInCred[formElementIdentifier].value
+        }
+
+
+        const signInInfo = formData
         axios.post('/signin', signInInfo)
         .then(response => {
             console.log(response.data)
+            this.setState({loading: false})
             this.props.history.push({pathname: 'allPosts'})
         })
         .catch(error => {
+            this.setState({loading: false})
             console.log(error)
         })
         // alert("Post submitted")
     }
 
     render() {
+
+        const formElementsArray = []
+        for (let key in this.state.signInCred) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.signInCred[key],
+            })
+        }
+
+        let form = (
+                    <Aux>
+                        {
+                            formElementsArray.map(formElement => (
+                                <Input 
+                                    key = {formElement.id} 
+                                    elementType={formElement.config.elementType}
+                                    elementConfig={formElement.config.elementConfig}
+                                    value={formElement.config.value} 
+                                    shouldValidate = {formElement.config.validation}
+                                    touched={formElement.config.touched}
+                                    inValid={!formElement.config.valid} 
+                                    changed={(event) => this.inputChangeHandler(event, formElement.id)} />
+                            ))
+                        }
+                        {/* <input 
+                            name="username" 
+                            placeholder="Username" 
+                            type="text"
+                            value={this.state.signInInfo.username}
+                            onChange={this.changeHandler}/>
+
+                        <input 
+                            id='pw' 
+                            name='password' 
+                            placeholder='Password' 
+                            type='password' 
+                            onChange={this.changeHandler}/> */}
+
+                        <button className={classes.Animated} onClick={this.submitClicked}>SignIn</button>
+                        <div className = {classes.Forgot}>Don't have an account?<Link to='/signup'> SignUp</Link></div>
+                    </Aux>
+                        
+        )
+
+        if(this.state.loading) {
+            form = <Spinner />
+        }
+
         return (
             <Aux>
                 <div className={classes.Cont}>
+                    <div className={classes.Login}>
+                        <h2>Sign In</h2>
+                        {form}
+                    </div>                    
+                </div>
+                {/* <div className={classes.Cont}>
                     <div className={classes.Login}>
                         <h2>
                             Sign In 
@@ -65,7 +208,7 @@ class SignIn extends Component {
                         <div className = {classes.Forgot}>Don't have an account?<Link to='/signup'> SignUp</Link></div>
 
                     </div>
-                </div>
+                </div> */}
             </Aux>
         )
     }
--- src/containers/NewPost/NewPost.js
+++ src/containers/NewPost/NewPost.js
@@ -3,32 +3,107 @@ import Aux from '../../hoc/Aux/Aux'
 import classes from './NewPost.module.css'
 import axios from '../../Axios/axios-post'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import Input from '../../components/UI/Input/Input'
 import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import Spinner from '../../components/UI/Spinner/Spinner'
+
 class NewPost extends Component {
+    // state = {
+    //     post: {
+    //         title: '',
+    //         desc: ''
+    //     },
+    //     submittable: false,
+    //     submitting: false
+    // }
+
     state = {
-        post: {
-            title: '',
-            desc: ''
+        newPost: {
+            title: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Enter Title'
+                },
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
+            },
+            desc: {
+                elementType: 'textarea',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Enter Description'
+                },
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
+            },
         },
-        submittable: false,
-        submitting: false
+        formIsValid: false,
+        loading: false
     }
 
-    changeHandler = (event) => {
-        const Post = {...this.state.post}        
-        const name = event.target.name
-        const val = event.target.value
+    checkValidity(value, rules) {
+        let isValid = true
 
-        if(name === 'title')
-        Post.title = val
+        if(rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        // if(rules.minLength) {
+        //     isValid = value.length >= rules.minLength && isValid;
+        // }
+
+        // if(rules.maxLength) {
+        //     isValid = value.length <= rules.maxLength && isValid;
+        // }
+
+        return isValid
+
+    }
+
+    inputChangeHandler = (event, inputIdentifier) => {
+        const updatedNewPostForm = {
+            ...this.state.newPost
+        }
+
+        const updatedNewPostElement = {
+            ...updatedNewPostForm[inputIdentifier]
+        }
+
+        updatedNewPostElement.value = event.target.value
+        updatedNewPostElement.valid = this.checkValidity(updatedNewPostElement.value, updatedNewPostElement.validation)
+        updatedNewPostElement.touched = true
+        updatedNewPostForm[inputIdentifier] = updatedNewPostElement
+
+        let formIsValid = true
+        for(let inputIdentifier in updatedNewPostForm) {
+            formIsValid = updatedNewPostForm[inputIdentifier].valid && formIsValid
+        }
+
+        this.setState({newPost: updatedNewPostForm, formIsValid: formIsValid})
+    }
+    // changeHandler = (event) => {
+    //     const Post = {...this.state.post}        
+    //     const name = event.target.name
+    //     const val = event.target.value
+
+    //     if(name === 'title')
+    //     Post.title = val
         
-        if(name === 'description')
-        Post.desc = val
+    //     if(name === 'description')
+    //     Post.desc = val
 
-        this.setState({post: Post})
+    //     this.setState({post: Post})
 
-    }
+    // }
 
     submitHandler = () => {
         let submit = false
@@ -42,29 +117,65 @@ class NewPost extends Component {
         this.setState({submittable: submit})
     }
 
-    submitClicked = () => {
-        this.setState({submitting: true})
-        const post = this.state.post
-        axios.post('/users/goyalanubhav11/posts', post)
+    submitClicked = (event) => {
+        event.preventDefault();
+        this.setState({loading: true})
+
+        const formData = {}
+
+        for (let formElementIdentifier in this.state.newPost) {
+            formData[formElementIdentifier] = this.state.newPost[formElementIdentifier].value
+        }
+
+        const newPost = formData
+        // this.setState({submitting: true})
+        // const post = this.state.post
+        axios.post('/users/vishal758/posts', newPost)
         // axios.post('https://burger-react-app-50038.firebaseio.com/post.json', post)
         .then(response => {
             console.log(response.data)
+            this.setState({loading: false})
+            this.props.history.push({pathname: 'allPosts'})
         })
         .catch(error => {
+            this.setState({loading: false})
             console.log(error)
         })
-        this.props.history.push({pathname: 'allPosts'})
         // alert("Post submitted")
     }
     render() {
 
-        // let submit = false
-        // let disInfo = {
-        //     ...this.state.post
-        // }
-        // for(let key in disInfo) {
-        //     if(disInfo[key] === "") submit = true
-        // }
+        const formElementArray = []
+        for(let key in this.state.newPost) {
+            formElementArray.push({
+                id: key,
+                config: this.state.newPost[key]
+            })
+        }
+        
+        let form = (
+            <form className={classes.Container} onSubmit={this.submitClicked}>
+                {
+                    formElementArray.map(formElement => (
+                        <Input 
+                            key = {formElement.id}
+                            elementType = {formElement.config.elementType}
+                            elementConfig = {formElement.config.elementConfig}
+                            value={formElement.config.value}
+                            shouldValidate={formElement.config.validation}
+                            touched={formElement.config.touched}
+                            inValid={!formElement.config.valid}
+                            changed={(event) => this.inputChangeHandler(event, formElement.id)} 
+                            from='newPost' />
+                    ))
+                }
+                <button disabled={!this.state.formIsValid}>SUBMIT POST</button>
+            </form>
+        )
+
+        if(this.state.loading) {
+            form = <Spinner />
+        }
 
         return (
             <Aux>
@@ -78,7 +189,8 @@ class NewPost extends Component {
                     {/* Description : {this.state.post.description} */}
                 </div>
                 <br />
-                <div className = {classes.Container}>
+                {form}
+                {/* <div className = {classes.Container}>
                     <label>
                         Title:                     
                     </label>
@@ -100,7 +212,7 @@ class NewPost extends Component {
                             required/> 
                     <button 
                         onClick={this.submitClicked}>Submit</button>
-                </div>
+                </div> */}
             </Aux>
 
         )
